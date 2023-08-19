cb88d680c8bb8ea4238b81511b1dc8a26e04d2a4
Himanshu Bhardwaz <himanshubhardwaz@Himanshus-MacBook-Air.local>
2023-08-19 21:10:55 +0530

    working but can use dom mutation observer instead of timeouts

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,7 @@
 // background.js
 // Listen for a custom event (e.g., "reloadExtension") from your content script or popup script.
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "reloadExtension") {
-    chrome.runtime.reload();
-  }
+  // if (message.action === "reloadExtension") {
+  //   chrome.runtime.reload();
+  // }
 });
--- src/content.ts
+++ src/content.ts
@@ -1,3 +1,62 @@
-if (window.location.pathname !== "/") {
-  console.log("HEY THIS IS WORKING");
+let currentTry = 1;
+const maxTry = 10;
+
+async function wait(time = 500): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    setTimeout(() => {
+      currentTry++;
+      try {
+        muteAudioAndVideo();
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    }, time);
+  });
 }
+
+function muteAudioAndVideo(): void {
+  const audioToggle = document.querySelector(
+    '[data-is-muted="false"][aria-label="Turn off microphone (⌘ + d)"]'
+  ) as HTMLLIElement;
+
+  const videoToggle = document.querySelector(
+    '[data-is-muted="false"][aria-label="Turn off camera (⌘ + e)"]'
+  ) as HTMLLIElement;
+
+  if (audioToggle) {
+    audioToggle.click();
+  } else {
+    throw new Error("Could not find audio toggle");
+  }
+
+  if (videoToggle) {
+    videoToggle.click();
+  } else {
+    throw new Error("Could not find video toggle");
+  }
+}
+
+function waitRecurcively(time: number): void {
+  console.log("GOING TO WAIT FOR: ", time, " ms");
+  if (currentTry < maxTry) {
+    wait(time)
+      .then(() => {
+        if (window.location.pathname !== "/") {
+          try {
+            muteAudioAndVideo();
+          } catch (error) {
+            throw error;
+          }
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        if (currentTry < maxTry) {
+          waitRecurcively(time * 1.5);
+        }
+      });
+  }
+}
+
+waitRecurcively(500);
